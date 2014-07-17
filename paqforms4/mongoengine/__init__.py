0dfce6c367396d794b2c299e35301adf0eb6f7c3
Ivan Kleshnin <ivan@paqmind.com>
2014-07-17 12:12:52 +0300

    Intention based filters (no model_class dependency again)

--- paqforms4/mongoengine/__init__.py
+++ paqforms4/mongoengine/__init__.py
@@ -1,12 +1,7 @@
 from .converters import *
-from .helpers import get_filters, get_sorts
 
 
 __all__ = (
     # CONVERTERS
     'ModelConverter',
-
-    # HELPERS
-    'get_filters',
-    'get_sorts',
 )
--- paqforms4/pymongo/helpers.py
+++ paqforms4/pymongo/helpers.py
@@ -1,6 +1,5 @@
 from decimal import Decimal
 from datetime import datetime, timedelta
-from mongoengine import BooleanField, StringField, IntField, FloatField, DecimalField, DateTimeField
 try:
     from functools import singledispatch
 except ImportError:
@@ -10,12 +9,12 @@ from ..converters import *
 
 
 @singledispatch
-def value_to_query(model_field, form_field, command, name, value, filters, tz):
+def value_to_query(converter, command, name, value, filters, tz):
     pass # Do nothing (keep filters as is)
 
 
-@value_to_query.register(StringField)
-def _(model_field, form_field, command, name, value, filters, tz):
+@value_to_query.register(StrConverter)
+def _(converter, command, name, value, filters, tz):
     def contains(name, value, filters):
         if value:
             filters[name] = {'$regex': value, '$options': '-i'} # TODO -i breaks index support
@@ -39,10 +38,10 @@ def _(model_field, form_field, command, name, value, filters, tz):
     locals()[command](name, value, filters)
 
 
-@value_to_query.register(IntField)
-@value_to_query.register(FloatField)
-@value_to_query.register(DecimalField)
-def _(model_field, form_field, command, name, value, filters, tz):
+@value_to_query.register(IntConverter)
+@value_to_query.register(FloatConverter)
+@value_to_query.register(DecimalConverter)
+def _(converter, command, name, value, filters, tz):
     def equals(name, value, filters):
         filters[name] = value
 
@@ -68,69 +67,72 @@ def _(model_field, form_field, command, name, value, filters, tz):
     locals()[command](name, value, filters)
 
 
-@value_to_query.register(DateTimeField)
-def _(model_field, form_field, command, name, value, filters, tz):
-    converter = form_field.meta.get('converter')
-    if isinstance(converter, DateTimeConverter): # check intention
-        # Treat datetime as datetime
-        def equals(name, value, filters):
-            if value:
-                samesec = datetime(value.year, value.month, value.day, value.hour, value.minute, value.second, tzinfo=tz)
-                nextsec = samesec + timedelta(seconds=1)
-                filters[name] = {'$gte': samesec, '$lt': nextsec}
+@value_to_query.register(DateTimeConverter)
+def _(converter, command, name, value, filters, tz):
+    # Treat datetime as datetime
+    def equals(name, value, filters):
+        if value:
+            samesec = datetime(value.year, value.month, value.day, value.hour, value.minute, value.second, tzinfo=tz)
+            nextsec = samesec + timedelta(seconds=1)
+            filters[name] = {'$gte': samesec, '$lt': nextsec}
 
-        def not_equals(name, value, filters):
-            if value:
-                samesec = datetime(value.year, value.month, value.day, value.hour, value.minute, value.second, tzinfo=tz)
-                nextsec = samesec + timedelta(seconds=1)
-                filters.setdefault('$or', []).extend(
-                    [{name: {'$lt': samesec}}, {name: {'$gte': nextsec}}]
-                )
+    def not_equals(name, value, filters):
+        if value:
+            samesec = datetime(value.year, value.month, value.day, value.hour, value.minute, value.second, tzinfo=tz)
+            nextsec = samesec + timedelta(seconds=1)
+            filters.setdefault('$or', []).extend(
+                [{name: {'$lt': samesec}}, {name: {'$gte': nextsec}}]
+            )
 
-        def empty(name, value, filters):
-            if value == 'yes':
-                filters[name] = None
-            elif value == 'no':
-                filters[name] = {'$ne': None}
+    def empty(name, value, filters):
+        if value == 'yes':
+            filters[name] = None
+        elif value == 'no':
+            filters[name] = {'$ne': None}
 
-        def between(name, value, filters):
+    def between(name, value, filters):
             if value['min'] and value['max']:
                 filters[name] = {'$gte': value['min'], '$lte': value['max']}
             elif value['min']:
                 filters[name] = {'$gte': value['min']}
             elif value['max']:
                 filters[name] = {'$lte': value['max']}
-    else:
-        # Treat datetime as date
-        def equals(name, value, filters):
-            if value:
-                sameday = datetime(value.year, value.month, value.day, tzinfo=tz)
-                nextday = sameday + timedelta(hours=24)
-                filters[name] =  {'$gte': sameday, '$lt': nextday}
 
-        def not_equals(name, value, filters):
-            if value:
-                sameday = datetime(value.year, value.month, value.day, tzinfo=tz)
-                nextday = sameday + timedelta(hours=24)
-                filters.setdefault('$or', []).extend(
-                    [{name: {'$lt': sameday}}, {name: {'$gte': nextday}}]
-                )
+    locals()[command](name, value, filters)
 
-        def between(name, value, filters):
-            if value['min'] and value['max']:
-                minday = datetime(value['min'].year, value['min'].month, value['min'].day, tzinfo=tz)
-                maxday = datetime(value['max'].year, value['max'].month, value['max'].day, tzinfo=tz)
-                maxday = maxday + timedelta(days=1)
-                filters[name] = {'$gte': minday, '$lt': maxday}
-            elif value['min']:
-                minday = datetime(value['min'].year, value['min'].month, value['min'].day, tzinfo=tz)
-                filters[name] = {'$gte': minday}
-            elif value['max']:
-                maxday = datetime(value['max'].year, value['max'].month, value['max'].day, tzinfo=tz)
-                maxday = maxday + timedelta(days=1)
-                filters[name] = {'$lt': maxday}
 
-        def empty(name, value, filters):
+@value_to_query.register(DateConverter)
+def _(converter, command, name, value, filters, tz):
+    # Treat datetime as date
+    def equals(name, value, filters):
+        if value:
+            sameday = datetime(value.year, value.month, value.day, tzinfo=tz)
+            nextday = sameday + timedelta(hours=24)
+            filters[name] =  {'$gte': sameday, '$lt': nextday}
+
+    def not_equals(name, value, filters):
+        if value:
+            sameday = datetime(value.year, value.month, value.day, tzinfo=tz)
+            nextday = sameday + timedelta(hours=24)
+            filters.setdefault('$or', []).extend(
+                [{name: {'$lt': sameday}}, {name: {'$gte': nextday}}]
+            )
+
+    def between(name, value, filters):
+        if value['min'] and value['max']:
+            minday = datetime(value['min'].year, value['min'].month, value['min'].day, tzinfo=tz)
+            maxday = datetime(value['max'].year, value['max'].month, value['max'].day, tzinfo=tz)
+            maxday = maxday + timedelta(days=1)
+            filters[name] = {'$gte': minday, '$lt': maxday}
+        elif value['min']:
+            minday = datetime(value['min'].year, value['min'].month, value['min'].day, tzinfo=tz)
+            filters[name] = {'$gte': minday}
+        elif value['max']:
+            maxday = datetime(value['max'].year, value['max'].month, value['max'].day, tzinfo=tz)
+            maxday = maxday + timedelta(days=1)
+            filters[name] = {'$lt': maxday}
+
+    def empty(name, value, filters):
             if value == 'yes':
                 filters[name] = None
             elif value == 'no':
@@ -139,23 +141,24 @@ def _(model_field, form_field, command, name, value, filters, tz):
     locals()[command](name, value, filters)
 
 
-def get_filters(filterform, model_class, tz=None):
+def get_filters(filterform, tz=None):
     filters = {}
     for name, form_field in filterform:
         if hasattr(form_field, 'prototypes'):
             if 'command' in form_field.prototypes:
                 command = form_field.value['command']
                 if command:
-                    value = form_field.value[command]
+                    value_to_query(
+                        form_field.meta['converter'], command, name, form_field.value[command], filters, tz
+                    )
             elif 'min' in form_field.prototypes and 'max' in form_field.prototypes:
-                command = 'between'
-                value = form_field.value
-            else:
-                command = None
-                value = None
-            if command:
-                model_field = model_class._fields[name]
-                value_to_query(model_field, form_field, command, name, value, filters, tz)
+                value_to_query(
+                    form_field.meta['converter'], 'between', name, form_field.value, filters, tz
+                )
+        elif form_field.meta.get('converter'):
+            value_to_query(
+                form_field.meta.get('converter'), 'equals', name, form_field.value, filters, tz
+            )
     return filters
 
 
