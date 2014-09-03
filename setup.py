74d13b171d5299a39818f7bfe7e7a7e67883385e
Ivan Kleshnin <ivan@paqmind.com>
2014-09-03 10:14:11 +0300

    Set license to MIT

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name = 'PaqForms',
+    name = 'paqforms',
     version = '4.5',
-    license = 'BSD',
+    license = 'MIT',
     requires = ['markupsafe', 'babel', 'jinja2'],
     packages = find_packages(),
     include_package_data = True,
